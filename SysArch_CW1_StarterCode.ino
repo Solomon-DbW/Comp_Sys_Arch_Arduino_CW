80cf3387edcdd8b81d83a324a5d7cc6b9fb0a25b
Solomon <161756396+Solomon-DbW@users.noreply.github.com>
2026-02-22 16:05:20 +0000

    Enhance Whack-a-Mole game with scoring and debounce
    Refactor game logic to include scoring and debounce functionality.

--- SysArch_CW1_StarterCode.ino
+++ SysArch_CW1_StarterCode.ino
@@ -1,40 +1,55 @@
-// assign LEDs and button to pins
-int ledPin[] = {4,5,6};
+int ledPin[] = {4, 5, 6};
 int playerOneButton = 2;
 int whiteLED = 9;
 
-// declare variables
-int delayTime; // time delay between lights on/off
-int randNumber;
-int whiteLEDOn; 
+int delayTime = 500; 
+volatile int score = 0;
+volatile int currentActiveLED = -1;
+volatile unsigned long lastButtonPress = 0; // For debouncing
 
-//setup interrupt, button input and LED outputs
 void setup() {
-  attachInterrupt(0, playerOneInput, FALLING); // specify interrupt routine
-  for (int i=0; i<3; i++){
+  pinMode(playerOneButton, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(playerOneButton), playerOneInput, FALLING); 
+  
+  for (int i = 0; i < 3; i++) {
     pinMode(ledPin[i], OUTPUT);
   }
-  pinMode(playerOneButton, INPUT);
   pinMode(whiteLED, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Whack-a-Mole Started! Reach 10 points.");
 }
 
-//run main program loop
 void loop() {
-  randNumber = random(3); // select a random number
-  digitalWrite(ledPin[randNumber], HIGH); // light the LED with this number
-  delay(delayTime);
-  digitalWrite(ledPin[randNumber], LOW);
-  delay(delayTime);
-  whiteLEDOn = digitalRead(whiteLED);
-  if(whiteLEDOn=HIGH){
-    digitalWrite(whiteLED, LOW);
-  }  //if whiteLED on = turn it off
-}
+  if (score >= 10) {
+    Serial.println("YOU WIN!");
+    for (int i = 0; i < 3; i++) digitalWrite(ledPin[i], HIGH);
+    digitalWrite(whiteLED, HIGH);
+    while(true); 
+  }
 
+  int randNumber = random(3); 
+  
+  currentActiveLED = ledPin[randNumber]; 
+  digitalWrite(currentActiveLED, HIGH); 
+  delay(delayTime);
+  
+  digitalWrite(currentActiveLED, LOW);
+  currentActiveLED = -1; 
+  
+  // Keep the white LED on briefly if they scored, then turn off
+  delay(100); 
+  digitalWrite(whiteLED, LOW);
+  delay(delayTime);
+}
 
 void playerOneInput() {
-  digitalWrite(whiteLED, HIGH); 
-  /*change this code so that white LED only switches on when button is pressed 
-  at the right time*/
+  unsigned long currentTime = millis();
+  // Debounce: Only allow a hit every 200ms
+  if (currentTime - lastButtonPress > 200) {
+    if (currentActiveLED != -1) {
+      score++; 
+      digitalWrite(whiteLED, HIGH);
+    }
+    lastButtonPress = currentTime;
+  }
 }
-
